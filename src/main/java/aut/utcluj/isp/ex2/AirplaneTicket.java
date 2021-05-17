1960a145ed51d981e0f93683e83d5b302eee5ff7
Petrisor Bogdan <bogdipetrisor28@yahoo.com>
2021-05-17 18:28:47 +0300

    ex 2 and ex 4(first)

--- src/main/java/aut/utcluj/isp/ex2/AirplaneTicket.java
+++ src/main/java/aut/utcluj/isp/ex2/AirplaneTicket.java
@@ -3,11 +3,18 @@ package aut.utcluj.isp.ex2;
 /**
  * @author stefan
  */
-public class AirplaneTicket {
+public class AirplaneTicket extends Ticket {
     private String destination;
+    private String customerName;
+    private Double price;
+    private String id;
 
     public AirplaneTicket(String id, String customerName, Double price, String destination) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        super(id,customerName,price);
+        this.destination = destination;
+        this.customerName = customerName;
+        this.price = price;
+        this.id = id;
     }
 
     public String getDestination() {
