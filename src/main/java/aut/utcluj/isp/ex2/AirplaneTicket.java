9f3680e61e8509d73b993a2a8f83d180c871795a
Petrisor Bogdan <bogdipetrisor28@yahoo.com>
2021-05-18 14:53:17 +0300

    editing ex2

--- src/main/java/aut/utcluj/isp/ex2/AirplaneTicket.java
+++ src/main/java/aut/utcluj/isp/ex2/AirplaneTicket.java
@@ -5,16 +5,11 @@ package aut.utcluj.isp.ex2;
  */
 public class AirplaneTicket extends Ticket {
     private String destination;
-    private String customerName;
-    private Double price;
-    private String id;
+
 
     public AirplaneTicket(String id, String customerName, Double price, String destination) {
         super(id,customerName,price);
         this.destination = destination;
-        this.customerName = customerName;
-        this.price = price;
-        this.id = id;
     }
 
     public String getDestination() {
